db0fa5c0b3f0ec9e77e324d9d1b9f195caace430
Deadpool1 <atwood169@yahoo.com>
2023-07-27 19:53:59 -0500

    added animals

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Greeting from './assets/components/Greeting'
 import Test from './assets/components/Test'
+import Animals from './assets/components/Animals'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
         <h1>Hello World</h1>
         <Greeting />
         <Test />
+        <Animals />
       </div>
     </>
   )
---
+++ src/assets/components/Animals.jsx
@@ -0,0 +1,15 @@
+const Animals = () => {
+    const animals = ["Lion", "Cow", "Snake", "Lizard", "Bull"]
+    const animalsList = animals.map((animal) => <li key={animal}>{animal}</li>)
+                    
+    return (
+        <div>
+            <h1>Animals: </h1>
+            <ul>
+                {animalsList}
+            </ul>
+        </div>
+    )
+}
+
+export default Animals
