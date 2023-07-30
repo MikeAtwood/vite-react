28425d7a4bce53e8db034092c0b1f315bf4935b7
Deadpool1 <atwood169@yahoo.com>
2023-07-30 13:37:03 -0500

    updated Item

--- src/assets/components/Item.jsx
+++ src/assets/components/Item.jsx
@@ -1,9 +1,10 @@
 // eslint-disable-next-line react/prop-types
 function Item({ name, isPacked}) {
-    if (isPacked) {
-        return <li className="item">{name} ✔</li>
-    }
-    return <li className="item">{name}</li>
+   return (
+    <li className="item">
+        {isPacked ? name + ' ✔' : name}
+    </li>
+   )
 }
 
 export default function PackingList() {
