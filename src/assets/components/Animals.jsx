e1d7f52b5793c378e6741e441290a675a009f4ba
Deadpool1 <atwood169@yahoo.com>
2023-07-30 13:14:20 -0500

    conditional rendering test

--- src/assets/components/Animals.jsx
+++ src/assets/components/Animals.jsx
@@ -1,5 +1,10 @@
 const Animals = () => {
     const animals = ["Lion", "Cow", "Snake", "Lizard", "Bull"]
+
+    if (animals.length === 0) {
+        return <div>No animals found.</div>
+    }
+
     const animalsList = animals.map((animal) => <li key={animal}>{animal}</li>)
                     
     return (
